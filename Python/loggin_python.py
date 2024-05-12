6a659056fbb362f27b250f831272d9f0876484ed
entbappy <entbappy73@gmail.com>
2024-05-12 12:09:21 +0600

    updated

---
+++ Python/loggin_python.py
@@ -0,0 +1,37 @@
+import logging
+import os
+
+#create a logger string
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_folder = "logs"
+log_file = "logs/running_log.log"
+
+os.makedirs(log_folder, exist_ok=True)
+
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
+        logging.FileHandler(log_file)
+    ]
+)
+
+logger = logging.getLogger("mylog")
+
+
+
+
+def add_number(a,b):
+    out = a + b
+    logger.info("Successfully executed")
+    print()
+
+    return out
+
+
+num = add_number(3,4)
+print(num)
+logger.info("code excuted")
